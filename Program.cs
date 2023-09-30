ede95d25928e3493e88f51a23df87973cd9f4686
pimplebb <ksemoro77@mail.ru>
2023-09-30 17:09:28 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 2
+// Console.Clear();
+// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+//     Console.WriteLine("{0} наибольшее число", a);
+// else
+//     Console.WriteLine("{0} наибольшее число", b);
+
+// Задача 4
+// Console.Clear();
+// Console.WriteLine("Введите первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число");
+// int c = Convert.ToInt32(Console.ReadLine());
+// int max = 0;
+// if (a > b) max = a;
+// if (b > c) max = b;
+// if (c > max) max = c;
+// Console.WriteLine("Максимальное значение имеет число" + " " + max);
+
+// Задача 6
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a % 2 == 0)
+//     Console.WriteLine("Число является четным");
+// else
+//     Console.WriteLine("Число является нечетным");
+
+// Задача 8
+// Console.Clear();
+// Console.WriteLine("Введите число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// for (int i = 1; i <= a; i++)
+//     if (i % 2 == 0)
+//         Console.Write(i + " ");
